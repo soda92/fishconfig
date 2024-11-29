3a8f25b49f061a61bcb82a2b7e1c029fda4b43af
sodacris <wjxa20152015@gmail.com>
2024-11-29 09:08:11 +0100

    add ubuntu settings

--- config.fish
+++ config.fish
@@ -2,17 +2,26 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# rvm
 set PATH $PATH /home/soda/.rvm/bin
 
 if type -f /home/soda/.rvm/bin/rvm 2>/dev/null 1> /dev/null
     rvm default
 end
 
+set -x EDITOR nvim
+if type -f ~/nvim.appimage 2>/dev/null 1>/dev/null
+    alias vim "~/nvim.appimage"
+    set -x EDITOR "~/nvim.appimage"
+end
+
 # Created by `pipx` on 2024-11-28 18:54:24
 set PATH $PATH /home/soda/.local/bin
 
 fish_config theme choose ayu\ Dark
-set -x EDITOR nvim
+
+# rust fish
+set PATH /usr/local/bin $PATH
 
 if test -f /proc/version
     if string match -r ".*microsoft.*" (cat /proc/version) > /dev/null
