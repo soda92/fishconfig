f45977abc016bf0527498e6d11b9b8f85a4fe06a
sodacris <wjxa20152015@gmail.com>
2024-11-29 08:45:42 +0100

    move settings to config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,22 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if type -f rvm
-    rvm default
-    end
 end
 
+set PATH $PATH /home/soda/.rvm/bin
+
+if type -f /home/soda/.rvm/bin/rvm > /dev/null
+    rvm default
+end
 
 # Created by `pipx` on 2024-11-28 18:54:24
 set PATH $PATH /home/soda/.local/bin
+
+fish_config theme choose ayu\ Dark
+
+if test -f /proc/version
+    if string match -r ".*microsoft.*" (cat /proc/version) > /dev/null
+        set -x http_proxy "http://172.29.128.1:7897"
+        set -x https_proxy $http_proxy
+    end
+end
+
